9f1ec21ad7299a82bdd2c1d177d9e13d73b821ae
Nicolas B <nicolasboulenc@gmail.com>
2019-05-10 18:52:07 +0100

    yet another commit

--- 2d_ray_tracer.js
+++ 2d_ray_tracer.js
@@ -1,104 +1,121 @@
+class Wall {
+
+	constructor(ax, ay, bx, by) {
+
+		this.ax = ax;
+		this.ay = ay;
+		this.bx = bx;
+		this.by = by;
+		this.is_lit = false;
+		this.hits = null;
+	}
+
+	hit(point, is_lit) {
+		
+		this.hits.push({x: point.x, y: point.y, is_lit: is_lit});
+	}
+}
+
 class Ray {
 
-	constructor(pos_x, pos_y, dir_x, dir_y) {
+	constructor(angle, magnitude=50) {
 
-		this.position_x = pos_x;
-		this.position_y = pos_y;
-		this.direction_x = dir_x;
-		this.direction_y = dir_y;
+		this.angle = angle;
+		// to fix, this is not magnitude???
+		this.direction_x = Math.cos(angle) * magnitude;
+		this.direction_y = Math.sin(angle) * magnitude;
 	}
 
 	// returns an xy or null
-	cast(walls) {
+	cast(origin, walls) {
 
-		let min_point = null;
-		let min_distance = Infinity;
-		const result = walls.forEach( wall => {
+		let intersects = [];
+		let closest_wall = null;
+		let closest_intersect = null;
+		let closest_distance = Infinity;
+		for(const wall of walls) {
 
-			const x1 = this.position_x;
-			const y1 = this.position_y;
-			const x2 = this.direction_x + this.position_x;
-			const y2 = this.direction_y + this.position_y;
+			const x1 = origin.x;
+			const y1 = origin.y;
+			const x2 = this.direction_x + origin.x;
+			const y2 = this.direction_y + origin.y;
+
 			const x3 = wall.ax;
 			const y3 = wall.ay;
 			const x4 = wall.bx;
 			const y4 = wall.by;
 
 			const d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-			if(d === 0) return;
+			if(d === 0) continue;
 
 			const t = ( (x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4) ) / d;
 			const u = -( (x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3) ) / d;
 
 			let point = null;
-			if(t >= 0 && t <= 1) {
+			if(t >= 0 && u >= 0 && u <= 1) {
 				point = { x: x1 + t * (x2 - x1), y: y1 + t * (y2 - y1) };
-			}
-			if(u >= 0 && u <= 1) {
-				point = { x: x1 + u * (x2 - x1), y: y1 + u * (y2 - y1) };
 			}
 
 			if(point !== null) {
-				let distance = (point.x - this.position_x) * (point.x - this.position_x) + 
-								(point.y - this.position_y) * (point.y - this.position_y);
-				if(distance < min_distance) {
-					min_point = { x: point.x, y: point.y };
-					min_distance = distance;
+				let distance = (point.x - origin.x) * (point.x - origin.x) + (point.y - origin.y) * (point.y - origin.y);
+				intersects.push({wall: wall, point: point, distance: distance});
+
+				if(distance < closest_distance) {
+					closest_wall = wall;
+					closest_intersect = { x: point.x, y: point.y };
+					closest_distance = distance;
 				}
 			}
+		}
+
+		if(closest_intersect === null) return null;
+
+		// add hits to walls
+		intersects.forEach((intersect) => {
+			if(intersect.distance === closest_distance) {
+				intersect.wall.is_lit = true;
+				intersect.wall.hit(intersect.point, true);
+			}
+			else {
+				intersect.wall.hit(intersect.point, false);				
+			}
 		});
-
-
-		if(min_point !== null) {
-			return segment(this.position_x, this.position_y, min_point.x, min_point.y);
-		}
-		else {
-			return null;
-		}
+		
+		const intersection = { x: closest_intersect.x, y: closest_intersect.y };
+		return intersection;
 	}
 }
 
 class Light {
 
-	constructor(x, y) {
+	constructor(x, y, rays_count) {
 
 		this.x = x;
 		this.y = y;
 		this.rays = [];
 
 		let rays_index = 0;
-		const rays_count = 10;
 		let angle = 0;
 		while(rays_index < rays_count) {
-			angle = Math.PI / 4;
-			const x = Math.cos(angle) * 10;
-			const y = Math.sin(angle) * 10;
-			this.rays.push(new Ray(this.x, this.y, x, y));
+			angle = Math.PI * 2 / rays_count * rays_index;
+			this.rays.push(new Ray(angle));
 			rays_index++;
 		}
-	}
-
-	set_position(x, y) {
-
-		this.x = x;
-		this.y = y;
-		this.rays.forEach(ray => {
-			ray.position_x = x;
-			ray_position_y = y;
-		});
 	}
 
 	// returns an array of segments
 	cast(walls) {
 
-		let rays = this.rays.map(ray => {
-			
-			let r = ray.cast(walls); 
-			return r;
-		});
+		const intersections = [];
 
-		return rays;
+		for(const ray of this.rays) {
+
+			const point = ray.cast({x: this.x, y: this.y}, walls);
+			if(point !== null) intersections.push(point);
+		}
+
+		return intersections;
 	}
 }
 
-function segment(ax, ay, bx, by) { return {ax: ax, ay: ay, bx: bx, by: by }; }
+function wall(ax, ay, bx, by) { return {ax: ax, ay: ay, bx: bx, by: by, is_lit: false }; }
--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const App = {
 	ctx: null,
 	light: null,
 	walls: null,
+	intersections_count: 0,
 	timer: 0,
 	frame_count: 0,
 	frame_per_seconds: 0
@@ -21,15 +22,26 @@ function setup() {
 	App.ctx.fillStyle = "rgb(192, 192, 192)";
 	App.ctx.font = "12px Lucida Console";
 
-	App.light = new Light(App.canvas.width / 2, App.canvas.height / 2);
+	App.light = new Light(0, 0, 16000);
 	App.walls = [];
 
-	let offset1 = 300;
-	let offset2 = 200;
-	App.walls.push( segment(offset1, offset2, offset2, offset1) );
-	App.walls.push( segment(App.canvas.width - offset1, offset2, App.canvas.width - offset2, offset1) );
-	App.walls.push( segment(App.canvas.width - offset1, App.canvas.height - offset2, App.canvas.width - offset2, App.canvas.height - offset1) );
-	App.walls.push( segment(offset1, App.canvas.height - offset2, offset2, App.canvas.height - offset1) );
+	// 4 walls
+	App.walls.push( new Wall(300, 200, 500, 200) );
+	App.walls.push( new Wall(App.canvas.width - 200, 300, App.canvas.width - 200, 500) );
+	App.walls.push( new Wall(300, App.canvas.height - 200, 500, App.canvas.height - 200) );
+	App.walls.push( new Wall(200, 300, 200, 500) );
+
+	// centre square
+	App.walls.push( new Wall(App.canvas.width / 2 - 10, App.canvas.height / 2 - 10, App.canvas.width / 2 + 10, App.canvas.height / 2 - 10) );
+	App.walls.push( new Wall(App.canvas.width / 2 + 10, App.canvas.height / 2 - 10, App.canvas.width / 2 + 10, App.canvas.height / 2 + 10) );
+	App.walls.push( new Wall(App.canvas.width / 2 + 10, App.canvas.height / 2 + 10, App.canvas.width / 2 - 10, App.canvas.height / 2 + 10) );
+	App.walls.push( new Wall(App.canvas.width / 2 - 10, App.canvas.height / 2 + 10, App.canvas.width / 2 - 10, App.canvas.height / 2 - 10) );
+
+	// canvas bounds
+	App.walls.push( new Wall(0, 0, App.canvas.width, 0) );
+	App.walls.push( new Wall(App.canvas.width, 0, App.canvas.width, App.canvas.height) );
+	App.walls.push( new Wall(App.canvas.width, App.canvas.height, 0, App.canvas.height) );
+	App.walls.push( new Wall(0, App.canvas.height, 0, 0) );
 
 	requestAnimationFrame(loop);
 }
@@ -37,16 +49,18 @@ function setup() {
 function mouse_move(evt) {
 
     let rect = App.canvas.getBoundingClientRect();
-	App.lightset_position(evt.clientX - rect.left, evt.clientY - rect.top);
+	App.light.x = evt.clientX - rect.left;
+	App.light.y = evt.clientY - rect.top;
 }
 
 function loop(timestamp) {
 
+	//clear canvas
 	App.ctx.clearRect(0, 0, App.canvas.width, App.canvas.height);
 
-	draw_walls(App.walls, App.ctx);
-	draw_light(App.light, App.walls, App.ctx);
+	draw_scene(App.light, App.walls, App.ctx);
 
+	// display fps
 	if( (App.frame_count % 10) === 0 ) {
 
 		const time = performance.now();
@@ -54,44 +68,113 @@ function loop(timestamp) {
 		App.timer = time;
 		App.frame_count = 0;
 	}
-	App.ctx.fillText(`FPS: ${App.frame_per_seconds}`, 1, 12);
+	App.ctx.fillText(`FPS: ${App.frame_per_seconds} Intersect: ${App.intersections_count}`, 1, 12);
 	App.frame_count++;
 
 	requestAnimationFrame(loop);
 }
 
+function draw_scene1(light, walls, ctx) {
 
-function draw_walls(walls, ctx) {
+	App.walls.forEach(wall => { wall.is_lit = false; });
 
-	ctx.strokeStyle = 'orange';
+	// draw light rays
+	ctx.strokeStyle = "rgba(255, 165, 0, 0.1)";
+	ctx.lineWidth = 12;
+
+	const intersections = light.cast(walls);
+	intersections.forEach(point => {
+		
+		ctx.beginPath();
+		ctx.moveTo(light.x, light.y);
+		ctx.lineTo(point.x, point.y);
+		ctx.closePath();
+		ctx.stroke();
+	});
+
+
+	// draw walls
+	ctx.strokeStyle = "grey";
+	ctx.lineWidth = 3;
 	ctx.beginPath();
 	
 	walls.forEach(wall => {
 
-		ctx.moveTo(wall.ax, wall.ay);
-		ctx.lineTo(wall.bx, wall.by);
+		if(wall.is_lit === true) {
+			ctx.moveTo(wall.ax, wall.ay);
+			ctx.lineTo(wall.bx, wall.by);			
+		}
 	});
 
 	ctx.closePath();
     ctx.stroke();
+
+
+	// draw light source
+	ctx.strokeStyle = "rgba(255, 0, 0, 0.5)";
+	ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+	ctx.lineWidth = 1;
+	
+	ctx.beginPath();
+	ctx.arc(light.x, light.y, 10, 0, 2 * Math.PI);
+	ctx.closePath();
+    ctx.fill();
+
+    ctx.stroke();
 }
 
 
-function draw_light(light, walls, ctx) {
+function draw_scene(light, walls, ctx) {
 
-	ctx.strokeStyle = 'orange';
+	App.walls.forEach(wall => { 
+		wall.is_lit = false; 
+		wall.hits = []; 
+	});
+
+	// draw light rays
+	ctx.fillStyle = "rgba(255, 165, 0, 6.0)";
+
 	ctx.beginPath();
 
-	ctx.arc(light.x, light.y, 10, 0, 2 * Math.PI);
-
-	const rays = light.cast(walls);
-
-	rays.forEach(ray => {
-
-		ctx.moveTo(ray.ax, ray.ay);
-		ctx.lineTo(ray.bx, ray.by);
+	const intersections = light.cast(walls);
+	App.intersections_count = intersections.length;
+	intersections.forEach((point, index) => {
+		if(index === 0) ctx.moveTo(point.x, point.y);
+		ctx.lineTo(point.x, point.y);
 	});
 
 	ctx.closePath();
+	ctx.fill();
+
+
+	// draw walls
+	ctx.strokeStyle = "grey";
+	ctx.lineWidth = 3;
+	ctx.beginPath();
+	
+	walls.forEach(wall => {
+		wall.hits.forEach((point, index) => {
+			if(index === 0) ctx.moveTo(point.x, point.y);
+			if(point.is_lit === false) 
+				ctx.moveTo(point.x, point.y);
+			else
+				ctx.lineTo(point.x, point.y);
+		});
+	});
+
+	ctx.closePath();
+	ctx.stroke();
+
+
+	// draw light source
+	ctx.strokeStyle = "rgba(255, 0, 0, 0.5)";
+	ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+	ctx.lineWidth = 1;
+	
+	ctx.beginPath();
+	ctx.arc(light.x, light.y, 10, 0, 2 * Math.PI);
+	ctx.closePath();
+    ctx.fill();
+
     ctx.stroke();
 }
